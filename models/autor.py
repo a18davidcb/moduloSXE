311f6aa403b6d7b71ecf07dba49aed5f019c6d1e
David Cancela <a18davidcb@iessanclemente.net>
2020-03-29 17:54:56 +0200

    Añadido número de libros publicados a autor

--- models/autor.py
+++ models/autor.py
@@ -6,3 +6,9 @@ class autor(models.Model):
     _inherit = 'res.partner'
     
     book_ids = fields.One2many('libros.libro', 'id', string='Libros escritos')
+    published_books = fields.Integer('Número de libros publicados', compute='_num_libros', store=False)
+
+    @api.depends('book_ids')
+    def _num_libros(self):
+        for record in self:
+            return len(self.book_ids)
