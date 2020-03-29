aa9596c2eff016010ffb46455dc55e6f587b6312
David Cancela <a18davidcb@iessanclemente.net>
2020-03-29 17:08:19 +0200

    Commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Libros",
+
+    'summary': """
+        Control de autores y libros escritos""",
+
+    'description': """
+        Controlar autores y los libros que hayan escrito
+    """,
+
+    'author': "a18davidcb",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
